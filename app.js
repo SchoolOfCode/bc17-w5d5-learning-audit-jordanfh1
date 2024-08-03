821ca97f1122150d3d01b8fdd05a30b6f18db9d0
Jordan <jordanfhoughton@gmail.com>
2024-08-03 17:41:36 +0100

    added env

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+//Create a route that sends a response to the client
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+
+
+//creat a delete route that deletes EVERYTHNG
+app.delete('/', (req, res) => {
+    res.send('EVERYTHING DELETED');
+    });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
